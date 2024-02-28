f1e49a8fbcb7c689796aca0bc62acd2538ce7c7d
betheljn <betheljn@jairusdesktop>
2024-02-28 14:39:57 -0500

    completed userSlice

---
+++ LanRoute/src/slice/commentsSlice.jsx
@@ -0,0 +1,9 @@
+import {createSlice} from "@reduxjs/toolkit";
+import { lanRouteApi } from "../api/lanRouteAPi";
+
+const commentSlice = createSlice({
+    name: "comment",
+    initialState: {
+        
+    }
+})
