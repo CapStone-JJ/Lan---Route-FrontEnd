8751e04da717019ea191ec1850277b98791ba8d9
betheljn <betheljn@jairusdesktop>
2024-02-28 11:57:39 -0500

    folders

---
+++ LanRoute/src/api/tags.js
@@ -0,0 +1,32 @@
+import { lanRouteApi } from "./api";
+
+const tagApi = lanRouteApi.injectEndpoints({
+  endpoints: (builder) => ({
+    getTags: builder.query({
+      query: () => "/api/tags",
+    }),
+    getTag: builder.query({
+      query: (id) => "/api/tags/" + id,
+    }),
+    addTag: builder.mutation({
+      query: (body) => ({
+        url: "/api/tags",
+        method: "POST",
+        body: body,
+      }),
+    }),
+    deleteTag: builder.mutation({
+      query: (id) => ({
+        url: "/api/tags/" + id,
+        method: "DELETE",
+      }),
+    }),
+  }),
+});
+
+export const {
+  useGetTagsQuery,
+  useGetTagQuery,
+  useAddTagMutation,
+  useDeleteTagMutation,
+} = tagApi;
