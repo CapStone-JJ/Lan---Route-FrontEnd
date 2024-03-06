2497745827cf772bdbe90970c09521a97cd69ed5
jgari008 <jgari008@DESKTOP-ILSIF5E>
2024-03-06 14:11:41 -0700

    complete components for comments and votes

--- LanRoute/src/components/Comments/Comment.jsx
+++ LanRoute/src/components/Comments/Comment.jsx
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
-import { useGetCommentsQuery, useAddCommentMutation } from "../../api/comments";
+import { useSelector } from "react-redux";
+import {
+  useGetCommentsQuery,
+  useAddCommentMutation,
+  useDeleteCommentMutation,
+} from "../../api/comments";
+import Votes from "./Votes";
 
 const Comment = ({ postId }) => {
   const [newCommentText, setNewCommentText] = useState("");
@@ -10,12 +16,21 @@ const Comment = ({ postId }) => {
     refetch,
   } = useGetCommentsQuery(postId);
   const [addComment] = useAddCommentMutation();
+  const [deleteComment] = useDeleteCommentMutation();
+  const userId = useSelector((state) => state.user.credentials.user.id);
+  console.log(userId);
 
   const handleAddComment = async () => {
     if (!newCommentText.trim()) return; // Check if the comment text is not just whitespace
     await addComment({ postId, text: newCommentText }).unwrap();
     refetch(); // Refetch comments after adding a new one
+    console.log(comments);
     setNewCommentText(""); // Clear the textarea after posting a comment
+  };
+
+  const handleDeleteComment = async (commentId) => {
+    await deleteComment(commentId).unwrap();
+    refetch(); // Refetch comments to update the list
   };
 
   if (isLoading) return <p>Loading comments...</p>;
@@ -26,7 +41,15 @@ const Comment = ({ postId }) => {
       <h3>Comments</h3>
       <div>
         {comments?.map((comment) => (
-          <p key={comment.id}>{comment.text}</p>
+          <div key={comment.id}>
+            <p>{comment.text}</p>
+            <Votes commentId={comment.id} userId={userId} />
+            {comment.userId === userId && ( // Only show the delete button for the user's own comments
+              <button onClick={() => handleDeleteComment(comment.id)}>
+                Delete Comment
+              </button>
+            )}
+          </div>
         ))}
       </div>
       <div>
