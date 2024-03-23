185bd5b2c5309b2cf73a478b77dd184883f3ad91
jgari008 <jgari008@DESKTOP-ILSIF5E>
2024-03-23 14:06:08 -0700

    update playlists

--- LanRoute/src/api/widgets.js
+++ LanRoute/src/api/widgets.js
@@ -1,99 +1,98 @@
 import { lanRouteApi } from "./lanRouteAPi";
 
 const widgetApi = lanRouteApi.injectEndpoints({
-    endpoints: (builder)=>({
-        getWidgets: builder.query({
-            query: ()=> '/api/widget',
-        }),
-        getSpecificWidget: builder.query({
-            query: (id) => '/api/widget/'+id   
-        }),
-        addWidget: builder.mutation({
-            query:(body)=>({
-                url:'/api/widget',
-                method:"POST",
-                body:body
-            })
-        }),
-        editWidget: builder.mutation({
-            query(data){
-                const {id, ...body}=data;
-                return {
-                    url: '/api/widget/'+id,
-                    method:"PUT",
-                    body
-                }
-            }
-        }),
-        deleteWidget:builder.mutation({
-            query:(id)=>({
-                url:'/api/widget/'+id,
-                method:'DELETE'
-            })
-        }),
-        getPlaylists: builder.query({
-            query: (category) => `/api/playlist/playlists/${category}`,
-            providesTags: ["Playlist"],
-
-        }),
-        getUserPlaylists: builder.query({
-            query: (userId) => `/api/playlist/user/${userId}`,
-        }),
-        getSpecificPlaylist: builder.query({
-            query: (id) => `api/playlist/${id}`,
-        }),
-        addPlaylist: builder.mutation({
-            query: (data) => {
-                const {body} =data;
-                return {
-                    url: `/api/playlist/`,
-                    method: 'POST',
-                    body,
-                }
-            },
-            invalidatedTags: ["Playlist"],
-        }),
-        editPlaylist: builder.mutation({
-            query: (data) => {
-                const {id, ...body} = data;
-                return {
-                    url:`/api/playlist/${id}`,
-                    method: 'PUT',
-                    body,
-                }
-            },
-            invalidatedTags: ["Playlist"],
-        }),
-        deletePlaylist: builder.mutation({
-            query: (id) => ({
-                url: `/api/playlist/${id}`,
-                method: 'DELETE',
-            }),
-            invalidatedTags: ["Playlist"],
-        }),
-        addToProfile: builder.mutation({
-            query: (playlistId) => ({
-                url: `/api/playlist/add-to-profile/${playlistId}`,
-                method: 'POST',
-            }),
-            invalidatedTags: ["Playlist"],
-        }),
-    })
-})
+  endpoints: (builder) => ({
+    getWidgets: builder.query({
+      query: () => "/api/widget",
+    }),
+    getSpecificWidget: builder.query({
+      query: (id) => "/api/widget/" + id,
+    }),
+    addWidget: builder.mutation({
+      query: (body) => ({
+        url: "/api/widget",
+        method: "POST",
+        body: body,
+      }),
+    }),
+    editWidget: builder.mutation({
+      query(data) {
+        const { id, ...body } = data;
+        return {
+          url: "/api/widget/" + id,
+          method: "PUT",
+          body,
+        };
+      },
+    }),
+    deleteWidget: builder.mutation({
+      query: (id) => ({
+        url: "/api/widget/" + id,
+        method: "DELETE",
+      }),
+    }),
+    getPlaylists: builder.query({
+      query: (category) => `/api/playlist/playlists/${category}`,
+      providesTags: ["Playlist"],
+    }),
+    getUserPlaylists: builder.query({
+      query: (userId) => `/api/playlist/user/${userId}`,
+    }),
+    getSpecificPlaylist: builder.query({
+      query: (id) => `api/playlist/${id}`,
+    }),
+    addPlaylist: builder.mutation({
+      query: (data) => {
+        const { body } = data;
+        return {
+          url: `/api/playlist`,
+          method: "POST",
+          body,
+        };
+      },
+      invalidatedTags: ["Playlist"],
+    }),
+    editPlaylist: builder.mutation({
+      query: (data) => {
+        const { id, ...body } = data;
+        return {
+          url: `/api/playlist/${id}`,
+          method: "PUT",
+          body,
+        };
+      },
+      invalidatedTags: ["Playlist"],
+    }),
+    deletePlaylist: builder.mutation({
+      query: (id) => ({
+        url: `/api/playlist/${id}`,
+        method: "DELETE",
+      }),
+      invalidatedTags: ["Playlist"],
+    }),
+    addToProfile: builder.mutation({
+      query: (playlistId) => ({
+        url: `/api/playlist/add-to-profile/${playlistId}`,
+        method: "POST",
+      }),
+      invalidatedTags: ["Playlist"],
+    }),
+  }),
+});
 
 export const {
-    useGetWidgetsQuery,
-    useGetSpecificWidgetQuery,
-    useAddWidgetMutation,
-    useEditWidgetMutation,
-    useDeleteWidgetMutation,
-    useGetPlaylistsQuery,
-    useGetUserPlaylistsQuery,
-    useGetSpecificPlaylistQuery,
-    useAddPlaylistMutation,
-    useEditPlaylistMutation,
-    useDeletePlaylistMutation,
-    useAddToProfileMutation,
-   } = widgetApi;
+  useGetWidgetsQuery,
+  useGetSpecificWidgetQuery,
+  useAddWidgetMutation,
+  useEditWidgetMutation,
+  useDeleteWidgetMutation,
+  useGetPlaylistsQuery,
+  useGetUserPlaylistsQuery,
+  useGetSpecificPlaylistQuery,
+  useAddPlaylistMutation,
+  useEditPlaylistMutation,
+  useDeletePlaylistMutation,
+  useAddToProfileMutation,
+} = widgetApi;
 
-   export default widgetApi;
+export default widgetApi;
