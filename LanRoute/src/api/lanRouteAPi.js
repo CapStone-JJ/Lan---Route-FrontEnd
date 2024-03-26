91d71765bb49039526d2bb1aaebc55d10c94ab95
betheljn <betheljn@jairusdesktop>
2024-03-26 17:32:50 -0400

    baseURL

--- LanRoute/src/api/lanRouteAPi.js
+++ LanRoute/src/api/lanRouteAPi.js
@@ -6,8 +6,8 @@ export const lanRouteApi = createApi({
   reducerPath: "lanRouteApi",
   baseQuery: fetchBaseQuery({
     baseUrl: 
-    // import.meta.env.VITE_URL || 
-    "http://localHost:3333",
+    import.meta.env.VITE_URL,
+    // "http://localHost:3333",
     prepareHeaders: (headers, { getState }) => {
       const credentials = JSON.parse(
         window.sessionStorage.getItem(CREDENTIALS)
